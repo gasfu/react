204056f27227c510157014f5faf95c35a18309c5
Gabriel Furlan <gabrielfurlan05@gmail.com>
2017-02-17 23:36:28 -0200

    Zerando o projeto

--- app/app.js
+++ app/app.js
@@ -1,8 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWord from './components/helloWord.jsx';
-
-ReactDOM.render(
-	<HelloWord />,
-	document.querySelector('#app')
-);
